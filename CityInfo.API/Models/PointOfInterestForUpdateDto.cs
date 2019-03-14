73f3409a5b538cb79ec6b8cbb6369571e2d77c34
Young Kim <younghyun87@gmail.com>
2019-03-14 15:49:55 -0400

    Module 4 End

--- CityInfo.API/Models/PointOfInterestForUpdateDto.cs
+++ CityInfo.API/Models/PointOfInterestForUpdateDto.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace CityInfo.API.Models
 {
-    public class PointOfInterestForCreationDto
+    public class PointOfInterestForUpdateDto
     {
-        [Required(ErrorMessage ="You should provide a name value.")]
+        [Required(ErrorMessage = "You should provide a name value.")]
         [MaxLength(50)]
         public string Name { get; set; }
 
