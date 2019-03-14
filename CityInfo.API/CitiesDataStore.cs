61fbadc592be94ce648f0ef12b372afe5d5f7198
young.kim@bell.ca <young.kim@bell.ca>
2019-03-14 11:29:42 -0400

    Initial Commit

---
+++ CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,54 @@
+﻿using CityInfo.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+        public List<CityDto> Cities { get; set; }
+
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "New York City",
+                    Description = "The one with that big park.",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 1,
+                            Name = "Central Park",
+                            Description = "That big park."
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 2,
+                            Name = "Empire State Building",
+                            Description = "That big tower."
+                        },
+                    }
+                },
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "Seoul",
+                    Description = "Korea's Capital City."
+                },
+                new CityDto()
+                {
+                    Id = 3,
+                    Name = "Toronto",
+                    Description = "Where I live."
+                }
+            };
+        }
+    }
+}
